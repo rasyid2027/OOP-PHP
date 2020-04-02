c0f5369efcee428b4a796a4034b7810b61de828b
rasyid2027 <halimurrasyid56@gmail.com>
2020-04-02 07:22:23 +0700

    fifth commit

---
+++ inharitance-concept.php
@@ -0,0 +1,71 @@
+<?php
+
+
+class Produk {
+    public  $judul,
+            $penulis,
+            $penerbit,
+            $harga,
+            $jmlHalaman,
+            $wktMain;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $wktMain = 0 ) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        $this->jmlHalaman = $jmlHalaman;
+        $this->wktMain = $wktMain;
+    }
+    
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk() {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga},-)";
+
+        return $str;
+    }
+
+
+}
+
+class Komik extends Produk{
+    public function getInfoProduk() {
+        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga},-) - {$this->jmlHalaman} Halaman.";
+        
+        return $str;
+    }
+}
+
+class Game extends Produk{
+    public function getInfoProduk() {
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga},-) ~ {$this->wktMain} Jam.";
+        
+        return $str;
+    }
+}
+
+
+class CetakInfoProduk {
+
+    public function cetak ( Produk $produk ) {
+        $str = "{$produk->judul} | {$produk->getLabel()}, (Rp. {$produk->harga})";
+        
+        return $str;
+    }
+
+}
+
+$produk1 = new Komik("One Piece", "Eichiro Oda", "Shonen Jump", 45000, 100, 0);
+$produk2 = new Game("Dota 2", "Lord Gaben", "Valve", 250000, 0, 50);
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
+
+
+
+
+
